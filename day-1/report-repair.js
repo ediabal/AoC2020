baec4f20da076d8fc019123145afd940935b5b1f
Logan Wholey <logan.wholey@fireeye.com>
2020-12-01 09:29:47 -0800

    day-1: part 2

--- day-1/report-repair.js
+++ day-1/report-repair.js
@@ -21,21 +21,27 @@ function findTwoNumbersThatSumAndMultiply(input, sum) {
 }
 
 function findThreeNumbersThatSumAndMultiply(input, sum) {
-  const map = input.reduce((acc, num, i) => {
-    acc[num] = i + 1;
-    return acc;
-  }, {});
-
   let product;
 
-  input.forEach((value, i) => {
-    let num = parseInt(value);
-    let remainder = sum - num;
-    if (map[remainder] && map[remainder] != i) {
-      product = num * remainder;
-      return;
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      if (j != i) {
+        for (let k = 0; k < input.length; k++) {
+          if (k != j && k != i) {
+            let [a, b, c] = [
+              parseInt(input[i]),
+              parseInt(input[j]),
+              parseInt(input[k]),
+            ];
+            if (a + b + c === sum) {
+              product = a * b * c;
+              break;
+            }
+          }
+        }
+      }
     }
-  });
+  }
 
   return product;
 }
@@ -44,8 +50,8 @@ module.exports = function reportRepair(sum = 2020) {
   try {
     const input = read('day-1/INPUT.md').trim().split('\n');
     return {
-      part1: findTwoNumbersThatSumAndMultiply(input, sum),
-      part2: findThreeNumbersThatSumAndMultiply(input, sum),
+      'part-1': findTwoNumbersThatSumAndMultiply(input, sum),
+      'part-2': findThreeNumbersThatSumAndMultiply(input, sum),
     };
   } catch (err) {
     console.error(err);
