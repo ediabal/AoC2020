72754c6223b3c08dcd26302a8b46a753d1587573
Logan Wholey <logan.wholey@fireeye.com>
2020-12-03 22:04:50 -0800

    day-4: spaghetti code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import passwordPhilosophy from './day-2/index.js';
 import tobogganTrajectory from './day-3/index.js';
 import day4 from './day-4/index.js';
 
-reportRepair();
-passwordPhilosophy();
-tobogganTrajectory();
+// reportRepair();
+// passwordPhilosophy();
+// tobogganTrajectory();
 day4();
