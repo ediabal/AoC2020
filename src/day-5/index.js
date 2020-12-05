da46fcbf19ab69abef661c22b47c6c32267931d0
Logan Wholey <logan.wholey@fireeye.com>
2020-12-04 20:04:25 -0800

    day-5: setup

---
+++ src/day-5/index.js
@@ -0,0 +1,23 @@
+import { read, log } from '../utils/io.js';
+
+export function formatInput(input) {
+  return input;
+}
+
+export function p1(input) {
+  return input;
+}
+
+export function p2(input) {
+  return input;
+}
+
+export default function day() {
+  try {
+    const input = read('src/day-5/INPUT.txt').trim().split('\n');
+    const formattedInput = formatInput(input);
+    log('day', p1(formattedInput), p2(formattedInput));
+  } catch (err) {
+    console.error(err);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import reportRepair from './day-1/index.js';
 import passwordPhilosophy from './day-2/index.js';
 import tobogganTrajectory from './day-3/index.js';
 import day4 from './day-4/index.js';
+import day5 from './day-5/index.js';
 
 // reportRepair();
 // passwordPhilosophy();
 // tobogganTrajectory();
-day4();
+// day4();
+day5();
