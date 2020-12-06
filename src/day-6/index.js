37ac40a8341dfcaaf01c5d8fac7e36ee04fa16be
Logan Wholey <logan.wholey@fireeye.com>
2020-12-05 21:52:39 -0800

    day-6

---
+++ src/day-6/index.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import { read, log } from '../utils/io.js';
+
+export function formatInput(input) {
+  const groups = input.map((g) => g.split('\n'));
+  return groups;
+}
+
+export function findSumOfUnionsInGroups(groups) {
+  return groups.reduce((sum, group) => {
+    return sum + _.union(...group.map((g) => g.split(''))).length;
+  }, 0);
+}
+
+export function findSumOfIntersectionInGroups(groups) {
+  return groups.reduce((sum, group) => {
+    return sum + _.intersection(...group.map((g) => g.split(''))).length;
+  }, 0);
+}
+
+export default function customCustoms() {
+  try {
+    const input = read('src/day-6/INPUT.txt').trim().split('\n\n');
+    const formattedInput = formatInput(input);
+    log(
+      'day',
+      findSumOfUnionsInGroups(formattedInput),
+      findSumOfIntersectionInGroups(formattedInput)
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import passwordPhilosophy from './day-2/index.js';
 import tobogganTrajectory from './day-3/index.js';
 import passportProcessing from './day-4/index.js';
 import binaryBoarding from './day-5/index.js';
+import customCustoms from './day-6/index.js';
 
 // reportRepair();
 // passwordPhilosophy();
 // tobogganTrajectory();
 // passportProcessing();
-binaryBoarding();
+// binaryBoarding();
+customCustoms();
